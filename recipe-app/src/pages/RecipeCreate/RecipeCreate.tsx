1c30607337fc93938355b0e9c1a0abff9a9357bf
acce8711 <101413675+acce8711@users.noreply.github.com>
2024-12-27 18:24:21 -0500

    Update RecipeCreate.tsx

--- recipe-app/src/pages/RecipeCreate/RecipeCreate.tsx
+++ recipe-app/src/pages/RecipeCreate/RecipeCreate.tsx
@@ -6,6 +6,12 @@ import useDurationManager from "./useDurationManager";
 import DragAndDrop from "../../shared components/DragAndDrop";
 import { TextField } from "@mui/material";
 
+import Checkbox from '@mui/material/Checkbox';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
+import CheckBoxIcon from '@mui/icons-material/CheckBox';
+
 import { Unstable_NumberInput as NumberInput } from '@mui/base/Unstable_NumberInput';
 
 const RecipeCreate = () => {
@@ -76,6 +82,32 @@ const RecipeCreate = () => {
             value={hours || ""}
             onChange={handleHourChange}/>
 
+            {/* Tags input */}
+            <Autocomplete
+                multiple
+                id="checkboxes-tags-demo"
+                options={top100Films}
+                disableCloseOnSelect
+                getOptionLabel={(option) => option.title}
+                renderOption={(props, option, { selected }) => {
+                    const { key, ...optionProps } = props;
+                    return (
+                    <li key={key} {...optionProps}>
+                        <Checkbox
+                        icon={icon}
+                        checkedIcon={checkedIcon}
+                        style={{ marginRight: 8 }}
+                        checked={selected}
+                        />
+                        {option.title}
+                    </li>
+                    );
+                }}
+                style={{ width: 500 }}
+                renderInput={(params) => (
+                    <TextField {...params} label="Checkboxes" placeholder="Favorites" />
+                )}
+            />
             
         </>
 
